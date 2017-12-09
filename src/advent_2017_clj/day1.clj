2907d4d2aefa07c7dca2fe4e754528a6214d467d
Nicholas Hansen <nick.m.hansen@gmail.com>
2017-12-09 03:21:42 -0800

    day 1 done

---
+++ src/advent_2017_clj/day1.clj
@@ -0,0 +1,28 @@
+(ns advent-2017-clj.day1
+  (:require [clojure.java.io :as io]))
+
+(def input
+  (-> "day1-input.txt"
+      io/resource
+      io/file
+      slurp
+      .trim))
+
+(def shifted-input
+  (apply str (concat (rest input) [(first input)])))
+
+(def part-1
+  (->> (map #(vector %1 %2) input shifted-input)
+       (filter (partial apply =))
+       (map (comp read-string str first))
+       (reduce +)))
+
+(def big-shifted-input
+  (let [shift (/ (count input) 2)]
+    (apply str (concat (drop shift input) (take shift input)))))
+
+(def part-2
+  (->> (map #(vector %1 %2) input big-shifted-input)
+       (filter (partial apply =))
+       (map (comp read-string str first))
+       (reduce +)))
