f89c09b184c57606abb4b5ec0ac3fc0e81552f08
Nick Hansen <n-hansen@users.noreply.github.com>
2018-12-02 20:41:55 -0800

    1b

--- hs/src/AoC2018/P1.hs
+++ hs/src/AoC2018/P1.hs
@@ -5,9 +5,13 @@ import Universum
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
+import Data.IntSet (member,insert)
 
 runPt1 :: Text -> IO ()
-runPt1 = print . sum . (:) 0 . parseInput
+runPt1 = print . sum . parseInput
+
+runPt2 :: Text -> IO ()
+runPt2 = print . findRepeatedFreq . parseInput
 
 type Input = [Int]
 
@@ -21,3 +25,12 @@ parseInput input =
   case parse inputParser "" input of
     Left bundle -> error . toText $ errorBundlePretty bundle
     Right p -> p
+
+findRepeatedFreq :: [Int] -> Int
+findRepeatedFreq = firstDupe mempty . scanl (+) 0 . cycle
+  where
+    firstDupe :: IntSet -> [Int] -> Int
+    firstDupe seen (this:rest) =
+      if this `member` seen
+      then this
+      else firstDupe (insert this seen) rest
--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -4,11 +4,12 @@ import Universum
 
 import System.Environment (getArgs)
 
-import qualified AoC2018.P1 as P1 (runPt1)
+import qualified AoC2018.P1 as P1 (runPt1,runPt2)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     ("1a":input:_) -> readFile input >>= P1.runPt1
+    ("1b":input:_) -> readFile input >>= P1.runPt2
     _ -> print "not sure what to do"
