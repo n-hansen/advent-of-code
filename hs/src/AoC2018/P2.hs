f3ed5fc3620e039686f42e6d2752c1982a79a5c1
Nick Hansen <n-hansen@users.noreply.github.com>
2018-12-03 20:55:15 -0800

    2b

--- hs/src/AoC2018/P2.hs
+++ hs/src/AoC2018/P2.hs
@@ -4,7 +4,7 @@ import Universum hiding (elems)
 
 import Data.HashMap.Strict (insertWith,elems)
 
-type Input = [[Char]]
+type Input = [String]
 
 parseInput :: Text -> Input
 parseInput = fmap toString . lines
@@ -15,3 +15,22 @@ runPt1 = print . computeChecksum . fmap countLetterReps . parseInput
     countLetterReps = foldl' (\repMap ltr -> insertWith (+) ltr 1 repMap) mempty
     computeChecksum repMaps = countReps 2 repMaps * countReps 3 repMaps
     countReps n = length . filter (not . null . filter (== n) . elems)
+
+runPt2 :: Text -> IO ()
+runPt2 = putStrLn . formatAnswer . computeAnswer . parseInput
+  where
+    computeAnswer :: Input -> (String, String)
+    computeAnswer [id1, id2] = (id1, id2)
+    computeAnswer (thisId:remainingIds) =
+      case filter (\otherId -> charDiff thisId otherId == 1) remainingIds of
+        [] -> computeAnswer remainingIds
+        (otherId:_) -> (thisId, otherId)
+
+    charDiff :: String -> String -> Int
+    charDiff id1 id2 = length . filter id $ zipWith (/=) id1 id2
+
+    formatAnswer :: (String, String) -> Text
+    formatAnswer (id1,id2) = unlines [ toText id1
+                                     , toText id2
+                                     , toText . map fst . filter (uncurry (==)) $ zip id1 id2
+                                     ]
--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -5,7 +5,7 @@ import Universum
 import System.Environment (getArgs)
 
 import qualified AoC2018.P1 as P1 (runPt1,runPt2)
-import qualified AoC2018.P2 as P2 (runPt1)
+import qualified AoC2018.P2 as P2 (runPt1,runPt2)
 import qualified AoC2018.P3 as P3 (runPt1,runPt2)
 
 main :: IO ()
@@ -15,6 +15,7 @@ main = do
     ("1a":input:_) -> readFile input >>= P1.runPt1
     ("1b":input:_) -> readFile input >>= P1.runPt2
     ("2a":input:_) -> readFile input >>= P2.runPt1
+    ("2b":input:_) -> readFile input >>= P2.runPt2
     ("3a":input:_) -> readFile input >>= P3.runPt1
     ("3b":input:_) -> readFile input >>= P3.runPt2
     _ -> print "not sure what to do"
