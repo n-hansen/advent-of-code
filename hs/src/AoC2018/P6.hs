c77e1fc5e6f7ca76046592d28a6dd11a88708453
Nick Hansen <n-hansen@users.noreply.github.com>
2018-12-06 21:24:19 -0800

    6a redux

--- hs/src/AoC2018/P6.hs
+++ hs/src/AoC2018/P6.hs
@@ -1,15 +1,10 @@
-{-# LANGUAGE DeriveAnyClass   #-}
-{-# LANGUAGE DeriveGeneric    #-}
 {-# LANGUAGE FlexibleContexts #-}
 module AoC2018.P6 (p6) where
 
 import           AoC2018
 import           Universum
 
-import           Data.Array                 (Array)
-import qualified Data.Array                 as A
-import           Data.Hashable
-import qualified Data.HashMap.Strict        as HM
+import qualified Data.MultiSet              as MS
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
@@ -28,86 +23,59 @@ inputParser = coordinate `endBy` newline
       y <- L.decimal
       pure (x,y)
 
-data Label = Unlabeled
-           | SingleLabel Char
-           | MultipleLabels
-           deriving (Show,Eq,Generic,Hashable)
-
-instance Container (Array i e) -- this is some bullshit
-
 pt1 :: Input -> Text
-pt1 input = show $ findLargestArea labeledGrid
+pt1 input = show
+            . maximum
+            . map snd
+            . frequencies
+            . map snd
+            . removeBoundaryLabels
+            $ labeledPoints
   where
     minX = minimum . fmap fst $ input
     maxX = maximum . fmap fst $ input
     minY = minimum . fmap snd $ input
     maxY = maximum . fmap snd $ input
-    minBd = (minX,minY)
-    maxBd = (maxX,maxY)
-    gridBds = (minBd,maxBd)
-    initialGrid = A.accumArray
-                  (const id)
-                  Unlabeled
-                  gridBds
-                  . zip input
-                  $ SingleLabel <$> ['a'..]
-    labeledGrid = computeAreas initialGrid
-    computeAreas grid =
-      let
-        neighborhood (x,y) = do
-          x' <- [x-1,x,x+1]
-          y' <- [y-1,y,y+1]
-          guard $ x == x' || y == y'
-          guard $
-            minX <= x' && x' <= maxX
-            && minY <= y' && y' <= maxY
-          pure (x',y')
-        label loc =
-          case grid A.! loc of
-            Unlabeled -> Just . (,) loc $
-              case filter (/= Unlabeled) . fmap (grid A.!) $ neighborhood loc of
-                []                          -> Unlabeled
-                (sl@(SingleLabel lbl):lbls) -> if all (== sl) lbls
-                                               then sl
-                                               else MultipleLabels
-                _                           -> MultipleLabels
-            _ -> Nothing
-      in
-        if all (/= Unlabeled) grid
-        then grid
-        else computeAreas
-             . (A.//) grid
-             . catMaybes
-             . fmap label
-             . A.indices
-             $ grid
-    findLargestArea = maximum . HM.elems . removeBoundaries . measureAreas
-    measureAreas = foldl'
-                   (\counts lbl ->
-                       case lbl of
-                         SingleLabel _ -> HM.alter (pure . maybe 1 (+ 1)) lbl counts
-                         MultipleLabels -> counts
-                   )
-                   mempty
-    removeBoundaries = HM.filterWithKey (\k _ -> not $ k `elem` boundaryLabels)
+    labeledInput = zip ['a'..] input
+    distancesAndCoords :: [(Coord, Distances)]
+    distancesAndCoords = do
+      x <- [minX..maxX]
+      y <- [minY..maxY]
+      pure ( (x,y)
+           , fmap (second $ manhattan (x,y)) labeledInput
+           )
+    labeledPoints :: [(Coord, Char)]
+    labeledPoints = catMaybes
+                    . fmap sequence -- sequence (x, Just y) => Just (x,y); sequence (x, Nothing) => Nothing
+                    . fmap ( second $ \dists ->
+                               case sortOn snd dists of
+                                 ((lbl,d1):(_,d2):_) | d1 == d2  -> Nothing
+                                                     | otherwise -> Just lbl
+                           )
+                    $ distancesAndCoords
+    boundaryLabels :: [Char]
     boundaryLabels = hashNub
-                     . fmap (labeledGrid A.!)
-                     $ [(x,y) | x <- [minX..maxX]
-                              , y <- [minY..maxY]
-                              , x == minX || x == maxX
-                                || y == minY || y == maxY]
-    -- pprintGrid =
-    --   toText . concat .
-    --   fmap (\((x,y),lbl) ->
-    --           (if y == 1 then "\n" else ""
-    --           ) <>
-    --           case lbl of
-    --             SingleLabel lbl -> [lbl]
-    --             _               -> "."
-    --           ) . A.assocs
+                     . fmap snd
+                     . filter (\((x,y),_) ->
+                                 x == minX || x == maxX ||
+                                 y == minY || y == maxY
+                              )
+                     $ labeledPoints
+    removeBoundaryLabels = filter (not . (`elem` boundaryLabels) . snd)
+
+type Coord = (Int,Int)
+type Distances = [(Char,Int)]
+
+manhattan :: Coord -> Coord -> Int
+manhattan (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
+
+frequencies :: Ord a => [a] -> [(a,Int)]
+frequencies = MS.toOccurList . MS.fromList
+
+
 
 
 
 exampleText = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9\n"
 exampleInput = fromMaybe [] . parseMaybe inputParser $ exampleText
-exampleOutput = pt1 exampleInput
+exampleOutput1 = pt1 exampleInput
