281f7645f8a5a5ceabda9fd110e211d274ab35a4
nick hansen <n-hansen@users.noreply.github.com>
2019-12-06 22:47:16 -0800

    cleanup tests a bit

--- hs-2019/src/Puzzles/P7.hs
+++ hs-2019/src/Puzzles/P7.hs
@@ -37,10 +37,6 @@ pt1 input = Just . maximum $ do
 pt2 input = Just . maximum $ do
   phaseConfig <- permutations [5..9]
   pure $ runFeedbackLoop input phaseConfig
-  where
-    -- input = unsafeFromJust $ parseMaybe inputParser
-    --         -- "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
-    --         "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
 
 
 runProgram st = let (status, st') = stepProgram st in if status == Continue then runProgram st' else st'
--- hs-2019/test/Spec.hs
+++ hs-2019/test/Spec.hs
@@ -15,6 +15,7 @@ import qualified Puzzles.P2 as P2
 import qualified Puzzles.P3 as P3
 import qualified Puzzles.P4 as P4
 import qualified Puzzles.P6 as P6
+import qualified Puzzles.P7 as P7
 
 main :: IO ()
 main = Test.Tasty.defaultMain =<< testSpec "advent-of-code-2019" spec
@@ -28,6 +29,7 @@ spec = parallel $ do
   puzzle4
   puzzle5
   puzzle6
+  puzzle7
 
 
 utilSpec :: Spec
@@ -55,6 +57,13 @@ utilSpec = describe "utils" $ do
     , (2,3,4)
     , (3,4,5)
     ]
+
+
+puzzleExample parser solver eg input expectation =
+  specify ("example " <> show eg) $
+  (parseMaybe parser input >>= solver)
+  `shouldBe`
+  Just expectation
 
 
 puzzle1 :: Spec
@@ -130,34 +139,21 @@ puzzle3 = describe "puzzle 3" $ do
          [(3,3), (6,5)]
 
     describe "provided examples" $ do
-      let runExample i1 i2 = parseMaybe P3.inputParser (i1 <> "\n" <> i2) >>= P3.pt1
-      specify "example 1" $
-        runExample "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83"
-        `shouldBe`
-        Just 159
-
-      specify "example 2" $
-        runExample "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-        `shouldBe`
-        Just 135
+      let example eg i1 i2 = puzzleExample P3.inputParser P3.pt1 eg (i1 <> "\n" <> i2)
+      example 1
+        "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83" 159
+      example 2
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" 135
 
   describe "part 2" $ do
     describe "provided examples" $ do
-      let runExample i1 i2 = parseMaybe P3.inputParser (i1 <> "\n" <> i2) >>= P3.pt2
-      specify "example 1" $
-        runExample "R8,U5,L5,D3" "U7,R6,D4,L4"
-        `shouldBe`
-        Just 30
-
-      specify "example 2" $
-        runExample "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83"
-        `shouldBe`
-        Just 610
-
-      specify "example 3" $
-        runExample "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-        `shouldBe`
-        Just 410
+      let example eg i1 i2 = puzzleExample P3.inputParser P3.pt2 eg (i1 <> "\n" <> i2)
+      example 1
+        "R8,U5,L5,D3" "U7,R6,D4,L4" 30
+      example 2
+        "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83" 610
+      example 3
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" 410
 
 
 puzzle4 :: Spec
@@ -180,8 +176,7 @@ puzzle5 = pure () -- TODO
 puzzle6 :: Spec
 puzzle6 = describe "puzzle 6" $ do
   describe "part 1" $ do
-    specify "example" $
-      let input = [r|COM)B
+    let input = [r|COM)B
 B)C
 C)D
 D)E
@@ -193,5 +188,24 @@ E)J
 J)K
 K)L
 |]
-          processed = parseMaybe P6.inputParser input >>= P6.pt1
-      in processed `shouldBe` Just 42
+    puzzleExample P6.inputParser P6.pt1 1 input 42
+
+
+puzzle7 :: Spec
+puzzle7 = describe "puzzle 7" $ do
+  describe "part 1" $ do
+    let example = puzzleExample P7.inputParser P7.pt1
+    example 1
+      "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" 43210
+    example 2
+      "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0" 54321
+    example 3
+      "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0" 65210
+
+  describe "part 2" $ do
+    let example = puzzleExample P7.inputParser P7.pt2
+    example 1
+      "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5" (Just 139629729)
+    example 2
+      "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+      (Just 18216)
