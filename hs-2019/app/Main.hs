1be842c36cb28d955714c55da50c53f60e5d8abc
nick hansen <n-hansen@users.noreply.github.com>
2019-12-06 22:31:18 -0800

    7

--- hs-2019/app/Main.hs
+++ hs-2019/app/Main.hs
@@ -7,11 +7,12 @@ import Puzzles.P3
 import Puzzles.P4
 import Puzzles.P5
 import Puzzles.P6
+import Puzzles.P7
 
 import System.Environment (getArgs)
 
 puzzles :: [Puzzle]
-puzzles = [p1,p2,p3,p4,p5,p6]
+puzzles = [p1,p2,p3,p4,p5,p6,p7]
 
 main :: IO ()
 main = do
---
+++ hs-2019/src/Puzzles/P7.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Puzzles.P7 where
+
+import Parse
+import Puzzle
+
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+
+
+data ComputerState = CS { _position :: Int
+                        , _inputStream :: [Int]
+                        , _outputStream :: [Int]
+                        , _tape :: Vector Int
+                        } deriving (Eq, Show)
+makeLenses ''ComputerState
+
+inputParser :: Parser Input
+inputParser = (flip $ CS 0 `flip` []) . V.fromList <$> (signedInteger `sepBy` "," <* optional newline)
+
+
+
+p7 :: Puzzle
+p7 = Puzzle "7" inputParser pt1 pt2
+
+type Input = [Int] -> ComputerState
+
+pt1 :: Input -> Maybe Int
+pt1 input = Just . maximum $ do
+  phaseConfig <- permutations [0..4]
+  pure $ foldr runAmp 0 phaseConfig
+  where
+    runAmp phaseSetting ampInput =
+      views outputStream unsafeHead . runProgram $ (input [phaseSetting, ampInput])
+
+
+pt2 input = Just . maximum $ do
+  phaseConfig <- permutations [5..9]
+  pure $ runFeedbackLoop input phaseConfig
+  where
+    -- input = unsafeFromJust $ parseMaybe inputParser
+    --         -- "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+    --         "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+
+
+runProgram st = let (status, st') = stepProgram st in if status == Continue then runProgram st' else st'
+
+runUntilBlocked st =
+  case stepProgram st of
+    (Continue, st') -> runUntilBlocked st'
+    ret -> ret
+
+runFeedbackLoop input phaserConfig = go [0] initAmps
+  where
+    initAmps = zipWith (\id phaserSetting -> (id, input [phaserSetting])) [0..] phaserConfig
+    go prevOutputs ((id,st):amps) =
+      case st & inputStream %~ (<> prevOutputs) & runUntilBlocked of
+        (Done, st') -> if id == 4 then st' ^? outputStream . ix 0
+                       else let nextOutputs = view outputStream st'
+                                st'' = st' & outputStream .~ mempty
+                            in go nextOutputs amps
+        (WaitForInput, st') -> let nextOutputs = view outputStream st'
+                                   st'' = st' & outputStream .~ mempty
+                               in go nextOutputs (amps <> [(id,st'')])
+
+data RunStatus = Continue | WaitForInput | Done
+               deriving (Eq,Show)
+
+stepProgram :: ComputerState -> (RunStatus, ComputerState)
+stepProgram st = eval . destructOpcode . unsafeFromJust $ arg 0
+  where
+    arg n = st ^? tape . ix (st ^. position . to (+ n))
+    deref i = st ^? tape . ix i
+    withMode 0 = deref
+    withMode 1 = Just . identity
+    advance n = position %~ (+ n)
+    writeOutput x = outputStream %~ (x:)
+    popInput = inputStream %~ unsafeTail
+    continue = (Continue,)
+    waitForInput = (WaitForInput, st)
+
+    eval :: (Int, [Int]) -> (RunStatus, ComputerState)
+    eval (1, mode1:mode2:0:_)
+      | Just val1 <- withMode mode1 =<< arg 1
+      , Just val2 <- withMode mode2 =<< arg 2
+      , Just out  <- arg 3
+      , Just _    <- deref out
+      = st
+        & tape . ix out .~ val1 + val2
+        & advance 4
+        & continue
+
+    eval (2, mode1:mode2:0:_)
+      | Just val1 <- withMode mode1 =<< arg 1
+      , Just val2 <- withMode mode2 =<< arg 2
+      , Just out  <- arg 3
+      , Just _    <- deref out
+      = st
+        & tape . ix out .~ val1 * val2
+        & advance 4
+        & continue
+
+    eval (3, 0:_)
+      | Just out <- arg 1
+      , Just _   <- deref out
+      , Just val <- st ^? inputStream . ix 0
+      = st
+        & popInput
+        & tape . ix out .~ val
+        & advance 2
+        & continue
+
+    eval (3, _)
+      | Nothing <- st ^? inputStream . ix 0
+      = waitForInput
+
+    eval (4, mode:_)
+      | Just val <- withMode mode =<< arg 1
+      = st
+        & writeOutput val
+        & advance 2
+        & continue
+
+    eval (5, mode1:mode2:_)
+      | Just val1 <- withMode mode1 =<< arg 1
+      , Just val2 <- withMode mode2 =<< arg 2
+      = st
+        & (if val1 /= 0 then position .~ val2 else advance 3)
+        & continue
+
+    eval (6, mode1:mode2:_)
+      | Just val1 <- withMode mode1 =<< arg 1
+      , Just val2 <- withMode mode2 =<< arg 2
+      = st
+        & (if val1 == 0 then position .~ val2 else advance 3)
+        & continue
+
+    eval (7, mode1:mode2:0:_)
+      | Just val1 <- withMode mode1 =<< arg 1
+      , Just val2 <- withMode mode2 =<< arg 2
+      , Just out  <- arg 3
+      , Just _    <- deref out
+      = st
+        & tape . ix out .~ (if val1 < val2 then 1 else 0)
+        & advance 4
+        & continue
+
+    eval (8, mode1:mode2:0:_)
+      | Just val1 <- withMode mode1 =<< arg 1
+      , Just val2 <- withMode mode2 =<< arg 2
+      , Just out  <- arg 3
+      , Just _    <- deref out
+      = st
+        & tape . ix out .~ (if val1 == val2 then 1 else 0)
+        & advance 4
+        & continue
+
+    eval (99, _) = (Done,st)
+
+    eval (o,ms) = error $ show (o,ms,st)
+
+destructOpcode opcode = (op, argModes <> repeat 0)
+  where
+    op = opcode `mod` 100
+    argModes = unfoldr (\number -> if number > 0
+                                   then Just (number `mod` 10, number `div` 10)
+                                   else Nothing)
+               $ opcode `div` 100
