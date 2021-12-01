876d0444d81a5392a64d18f89be5fac4e26ff7cf
nick hansen <n-hansen@users.noreply.github.com>
2021-12-01 16:15:27 -0700

    clean up templating

--- hs-2021/setup_problem.clj
+++ hs-2021/setup_problem.clj
@@ -3,16 +3,18 @@
 (def aoc-year 2021)
 
 (require '[babashka.curl :as curl]
-         '[babashka.fs :as fs])
+         '[babashka.fs :as fs]
+         '[selmer.parser :as selmer])
 
 (def session-id
   (slurp ".session_id"))
-
-(def puzzle-template
-  (slurp "src/Puzzles/Template.hs"))
-
+(def solution-template
+  (slurp "templates/Solution.template"))
 (def main-template
-  (slurp "app/Template.hs"))
+  (slurp "templates/Main.template"))
+(def test-template
+  (slurp "templates/Test.template"))
+
 
 (doseq [day *command-line-args*
         :let [{:keys [status body]}
@@ -21,22 +23,18 @@
   (assert (= status 200) (format "Bad response for day %s: %s\n%s" day status body))
   (spit (str "inputs/" day) body)
   (spit (format "src/Puzzles/P%s.hs" day)
-        (-> puzzle-template
-            (str/replace #"module Puzzles\.Template" (str "module Puzzles.P" day))
-            (str/replace #"PUZZLE" (str day)))))
+        (selmer/render solution-template {:day day}))
+  (spit (format "test/P%sSpec.hs" day)
+        (selmer/render test-template {:day day})))
 
 (def puzzles
   (->> (fs/glob "src/Puzzles" "P*.hs")
        (keep #(second (re-find #"P([0-9]+)\.hs" (str %))))))
 
 (spit "app/Main.hs"
-      (-> main-template
-          (str/replace #"\{-PUZZLE_IMPORTS-\}"
-                       (->> puzzles
-                            (map #(str "import Puzzles.P" %))
-                            (str/join \newline)))
-          (str/replace #"\{-PUZZLE_FNS-\}"
-                       (->> puzzles
-                            (map #(str "p" %))
-                            (str/join ",")))
-          (str/replace #"module Template" "module Main")))
+      (selmer/render main-template {:imports (->> puzzles
+                                                  (map #(str "import Puzzles.P" %))
+                                                  (str/join \newline))
+                                    :vals (->> puzzles
+                                               (map #(str "p" %))
+                                               (str/join ","))}))
