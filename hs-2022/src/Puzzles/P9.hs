97ccbc80a61148cf9c070fb9c74fffa7711a2164
nick hansen <nick@nickhansen.dev>
2022-12-10 10:16:33 -0700

    day 9 part 2

--- hs-2022/src/Puzzles/P9.hs
+++ hs-2022/src/Puzzles/P9.hs
@@ -4,8 +4,10 @@ import AocPrelude
 import Parse
 import Puzzle
 
+import Control.Monad.Trans.Maybe
 import Control.Monad.RWS.Strict
 import qualified Data.Set as Set
+import qualified Data.Sequence as Seq
 
 type Instruction = (Direction,Int)
 data Direction = U | D | L | R deriving (Eq,Show)
@@ -33,34 +35,39 @@ inputParser2 = inputParser1
 headLoc = _1
 tailLoc = _2
 
-step :: Direction -> RWS () (Set Coord) (Coord,Coord) ()
+step :: Direction -> RWS () (Set Coord) (Seq Coord) ()
 step dir = do
+  ropeLength <- gets Seq.length
+
   -- move head
   case dir of
-    U -> headLoc . _2 %= (+ 1)
-    D -> headLoc . _2 %= subtract 1
-    L -> headLoc . _1 %= (+ 1)
-    R -> headLoc . _1 %= subtract 1
+    U -> ix 0 . _2 %= (+ 1)
+    D -> ix 0 . _2 %= subtract 1
+    L -> ix 0 . _1 %= (+ 1)
+    R -> ix 0 . _1 %= subtract 1
 
-  -- move tail
-  (hx,hy) <- use headLoc
-  (tx,ty) <- use tailLoc
-  let dx = hx - tx
-      dy = hy - ty
-  when (abs dx >= 2 || abs dy >= 2) $ do
-    tailLoc . _1 %= (+ signum dx)
-    tailLoc . _2 %= (+ signum dy)
-    use (tailLoc . to Set.singleton) >>= tell
+  let moveKnot :: Int -> MaybeT (RWS () (Set Coord) (Seq Coord)) ()
+      moveKnot n = when (n < ropeLength) $ do
+        Just (x0,y0) <- preuse $ ix (n - 1)
+        Just (x1,y1) <- preuse $ ix n
+        let dx = x0 - x1
+            dy = y0 - y1
+        when (abs dx >= 2 || abs dy >= 2) $ do
+          ix n %= bimap (+ signum dx) (+ signum dy)
+          moveKnot $ n + 1
+  runMaybeT $ moveKnot 1
+  use (ix (ropeLength - 1) . to Set.singleton) >>= tell
 
-run :: [Instruction] -> Set Coord
-run prgm = snd $ execRWS (tell (Set.singleton startLoc) >> go prgm) () (startLoc,startLoc)
+run :: Int -> [Instruction] -> Set Coord
+run ropeLength prgm = snd $ execRWS (tell (Set.singleton startLoc) >> go prgm) () initRope
   where
     startLoc = (0,0)
+    initRope = Seq.replicate ropeLength startLoc
     go [] = pure ()
     go ((_,0):rest) = go rest
     go ((d,n):rest) = step d >> go ((d,n-1):rest)
 
 
-pt1 = Just . length . run
+pt1 = Just . length . run 2
 
-pt2 _ = Nothing :: Maybe ()
+pt2 = Just . length . run 10
--- hs-2022/test/P9Spec.hs
+++ hs-2022/test/P9Spec.hs
@@ -21,4 +21,17 @@ R 2
 |]
 
     pt1 input 13
-    -- pt2 input ???
+    pt2 input 1
+
+  describe "example 2" $ do
+    let input = [r|R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+|]
+
+    pt2 input 36
